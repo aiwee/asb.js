18b2c7e981c3b0567fedc498fb661fdceca329fb
Alex Potoccy <apotoccy@gmail.com>
2019-03-28 09:21:14 -0700

    main methods update

--- src/asb.js
+++ src/asb.js
@@ -1,3 +1,25 @@
+/*
+    Data stores in the JSON format:
+    localStorage.setItem(domain, data);
+    data = {
+        { "record01":
+             { "property01": "value" },
+             { "property02": "value" }.
+             ...
+        },
+        { "record02":
+             { "property01": "value" },
+             { "property02": "value" },
+             ...
+        },
+        ...
+    };
+    @domain - name of data table
+    @recordXX - unique name of record in the domain
+    @propertyXX - unique name of property in each record
+    @value - any value of property
+ */
+
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD
@@ -40,54 +62,97 @@
         },
     };
     var asb = {
+        /*
+            Creates empty object in the local storage with key @name
+         */
         addDomain: function(name) {
             util.quit(name);
             localStorage.setItem(name, JSON.stringify({}));
         },
+        /*
+            Creates new record in the @domain
+         */
         addRecord: function(domain, record) {
             util.quit(domain, record);
             var data = asb.getData(domain);
             data[record] = {};
             asb.saveData(domain, data);
         },
+        /*
+            Creates new property in the record
+         */
         addProperty: function(domain, record, property) {
             util.quit(domain, record, property);
             var data = asb.getData(domain);
-            data[record][property] = {};
+            data[record][property] = "";
             asb.saveData(domain, data);
         },
+        /*
+            Gets domain by @name from the localStorage
+         */
         getDomain: function(name) {
             util.quit(name);
             return localStorage.getItem(name);
         },
+        /*
+            Gets data from local storage as JSON object
+         */
         getData: function(domain) {
             util.quit(domain);
             return JSON.parse(asb.getDomain(domain));
         },
+        /*
+            Saves data to the domain
+         */
+        // ToDo: maybe updateDomain method will be enough
         saveData: function(domain, data) {
             util.quit(domain, data);
             asb.updateDomain(domain, data);
         },
+        /*
+            Updates domain in the local storage
+         */
         updateDomain: function(domain, data) {
             util.quit(domain, data);
             localStorage.setItem(domain, JSON.stringify(data));
         },
+        /*
+            Updates record in the domain
+         */
         updateRecord: function(domain, record, property) {
-            util.quit(domain, record);
-            property = util.norm(property);
-            property = util.prepare(property);
+            util.quit(domain, record, property);
             var data = asb.getData(domain);
             data[record] = property;
             asb.saveData(domain, data);
         },
+        /*
+            Updates property in the record
+         */
         updateProperty: function(domain, record, property, value) {
-            util.quit(domain, record, property);
-            value = util.norm(value);
-            value = util.prepare(value);
+            util.quit(domain, record, property, value);
             var data = asb.getData(domain);
             data[record][property] = value;
             asb.saveData(domain, data);
         },
+        pushData: function(domain, record, property, value) {
+            util.quit(domain, record, property, value);
+            if (!localStorage.getItem(domain))
+                asb.addDomain(domain);
+            var data = asb.getData(domain);
+
+            if (!data.hasOwnProperty(record))
+                asb.addRecord(domain, record);
+
+            data = asb.getData(domain);
+
+            if (!data[record].hasOwnProperty(property))
+                asb.addProperty(domain, record, property);
+
+            data = asb.getData(domain);
+
+            data[record][property] = value;
+            asb.saveData(domain, data);
+        }
     };
     return asb;
 }));
